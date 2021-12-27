26e19410bb85bcbf04a0f98cdbacd700ca11214c
Fabian Tan <ftan14169@gmail.com>
2021-12-27 21:42:22 +0100

    Add raw login page without validation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,61 @@
 import * as React from 'react';
-import type { NextPage } from 'next';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {NextPage} from 'next';
+
+const theme = createTheme();
 
 const Home: NextPage = () => {
-  return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          Homepage
-        </Typography>
-      </Box>
-    </Container>
-  );
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		// eslint-disable-next-line no-console
+		console.log({
+			email: data.get('email')
+		});
+	};
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							name="email"
+							autoComplete="email"
+							autoFocus
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Login
+						</Button>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	);
 };
 
 export default Home;
