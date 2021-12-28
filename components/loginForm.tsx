113bfa900b183ff74e4072ff6eb233054f6034ea
Fabian Tan <ftan14169@gmail.com>
2021-12-28 10:55:07 +0100

    Add LoginForm component with formik and yup

---
+++ components/loginForm.tsx
@@ -0,0 +1,63 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import * as React from 'react';
+import {useFormik} from 'formik';
+import * as yup from 'yup';
+
+const validationSchema = yup.object({
+	email: yup
+		.string()
+		.email('Enter a valid email')
+		.required('Email is required'),
+});
+
+const LoginForm = () => {
+
+	const formik = useFormik({
+		initialValues: {email: ''},
+		validationSchema: validationSchema,
+		onSubmit: (values) => {
+			alert(JSON.stringify(values, null, 2));
+		},
+	});
+
+	return (
+		<Box
+			sx={{
+				marginTop: 8,
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+			}}
+		>
+			<form onSubmit={formik.handleSubmit}>
+				<TextField
+					margin="normal"
+					required
+					fullWidth
+					id="email"
+					label="Enter your email address"
+					name="email"
+					autoComplete="email"
+					placeholder="example@email.com"
+					autoFocus
+					value={formik.values.email}
+					onChange={formik.handleChange}
+					error={formik.touched.email && Boolean(formik.errors.email)}
+					helperText={formik.touched.email && formik.errors.email}
+				/>
+				<Button
+					type="submit"
+					fullWidth
+					variant="contained"
+					sx={{mt: 3, mb: 2}}
+				>
+					Login
+				</Button>
+			</form>
+		</Box>
+	)
+}
+
+export default LoginForm;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,19 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {NextPage} from 'next';
+import LoginForm from '../components/loginForm';
 
 const theme = createTheme();
 
 const Home: NextPage = () => {
 
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		const data = new FormData(event.currentTarget);
-		// eslint-disable-next-line no-console
-		console.log({
-			email: data.get('email')
-		});
-	};
-
 	return (
 		<ThemeProvider theme={theme}>
 			<Container component="main" maxWidth="xs">
 				<CssBaseline />
-				<Box
-					sx={{
-						marginTop: 8,
-						display: 'flex',
-						flexDirection: 'column',
-						alignItems: 'center',
-					}}
-				>
-					<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-						<TextField
-							margin="normal"
-							required
-							fullWidth
-							id="email"
-							label="Email Address"
-							name="email"
-							autoComplete="email"
-							autoFocus
-						/>
-						<Button
-							type="submit"
-							fullWidth
-							variant="contained"
-							sx={{ mt: 3, mb: 2 }}
-						>
-							Login
-						</Button>
-					</Box>
-				</Box>
+				<LoginForm />
 			</Container>
 		</ThemeProvider>
 	);
