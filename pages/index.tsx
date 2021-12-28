ffcb336f399f5b47c8f4daf087915423ccb434d1
Fabian Tan <ftan@polaris.senacor.com>
2021-12-28 16:23:16 +0100

    Move welcome row to header.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,35 +3,18 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {NextPage} from 'next';
-import {useAuth} from '../src/authContext';
 import withAuthentication from '../src/withAuthentication';
-import Link from 'next/link';
+import Header from '../components/header';
 
 const theme = createTheme();
 
 const Home: NextPage = () => {
 
-	const {user, logout} = useAuth();
-
 	return (
 		<ThemeProvider theme={theme}>
 			<Container component="main" maxWidth="xs">
 				<CssBaseline/>
-				<div>
-					<h2>{user && user.email ? (
-						<>
-							<div>Welcome {user.email} </div>
-							<button onClick={logout}>Logout</button>
-						</>
-					) : (
-						<div>
-							<Link href='/login'>
-								<a>Please login</a>
-							</Link>
-						</div>
-					)
-					}</h2>
-				</div>
+				<Header />
 			</Container>
 		</ThemeProvider>
 	);
