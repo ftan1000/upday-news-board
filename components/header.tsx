5a2b9fa29806ce284b51368a344cc212f1f89bdf
Fabian Tan <ftan@polaris.senacor.com>
2021-12-29 13:43:36 +0100

    Add AppBar to header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,27 +1,72 @@
 import * as React from 'react';
 import {useAuth} from '../src/authContext';
-import Link from 'next/link';
+import {AppBar} from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import {AccountCircle} from '@mui/icons-material';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import Box from '@mui/material/Box';
 
 const Header = () => {
 
 	const {user, logout} = useAuth();
 
+	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+	const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
 	return (
 		<header>
-			{user && user.email ? (
-				<>
-					Welcome {user.email}
-					{' '}
-					<button onClick={logout}>Logout</button>
-				</>
-			) : (
-				<div>
-					<Link href='/login'>
-						<a>Please login</a>
-					</Link>
-				</div>
-			)
-			}
+			<Box sx={{ flexGrow: 1 }}>
+				<AppBar position="static">
+					<Toolbar>
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+							News Board
+						</Typography>
+						{user && user.email && (
+							<div>
+								{user.email}
+								<IconButton
+									size="large"
+									aria-label="account of current user"
+									aria-controls="menu-appbar"
+									aria-haspopup="true"
+									onClick={handleMenu}
+									color="inherit"
+								>
+									<AccountCircle />
+								</IconButton>
+								<Menu
+									id="menu-appbar"
+									anchorEl={anchorEl}
+									anchorOrigin={{
+										vertical: 'top',
+										horizontal: 'right',
+									}}
+									keepMounted
+									transformOrigin={{
+										vertical: 'top',
+										horizontal: 'right',
+									}}
+									open={Boolean(anchorEl)}
+									onClose={handleClose}
+								>
+									<MenuItem onClick={logout}>Logout</MenuItem>
+								</Menu>
+							</div>
+						)
+						}
+					</Toolbar>
+				</AppBar>
+			</Box>
 		</header>
 	)
 }
