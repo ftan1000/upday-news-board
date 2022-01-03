f576a65c4e99b0d82929e1f3b146536465efb447
Fabian Tan <ftan14169@gmail.com>
2022-01-03 22:50:11 +0100

    Improve newsItem.tsx

--- components/newsItem.tsx
+++ components/newsItem.tsx
@@ -1,20 +1,63 @@
 import * as React from 'react';
 import {Box} from '@mui/material';
 import {News} from '../src/types';
+import Link from 'next/link';
 
 const NewsItem = (props: {
 	data: News;
 }) => {
 
+	const DESCRIPTION_MAX_DISPLAY_LENGTH = 100;
+
 	const news: News = props.data;
 
+	const formattedDate = new Intl.DateTimeFormat('default', {
+		year: 'numeric', month: 'long', day: 'numeric',
+		hour: 'numeric', minute: 'numeric', second: 'numeric',
+		hour12: false,
+		timeZone: 'Europe/Berlin'
+	}).format(new Date(news.CreatedAt))
+
 	return (
-		<Box component='div' sx={{border: 1, padding: 3, margin: 2}} key={news.id}>
-			<h1>{news.title}</h1>
-			<img src={news.imageURL} alt={news.title}/>
-			<div>{news.description}</div>{/* TODO: Truncate description here when too long */}
-			<hr />
-			<sub>{news.author} - {news.CreatedAt}</sub> {/* TODO: Better date/time format */}
+		<Box component='div' sx={{marginTop: 5}} key={news.id}>
+			<Box component='div' sx={{border: 1, padding: 2}}>
+
+				<Box>
+					{news.title && (
+						<h1>{news.title}</h1>
+					)}
+					{!news.title && (
+						<em>No title</em>
+					)}
+				</Box>
+
+				<Box>
+					{news.imageURL && (
+						<img src={news.imageURL} alt={news.title}/>
+					)}
+					{!news.imageURL && (
+						<em>No imageURL</em>
+					)}
+				</Box>
+
+				<Box>
+					{news.description && (
+						<Box component='div' sx={{whiteSpace: 'pre-line'}}>
+							{news.description.substring(0, DESCRIPTION_MAX_DISPLAY_LENGTH)}
+							{news.description.length >= DESCRIPTION_MAX_DISPLAY_LENGTH && ` [...] (100 more characters)`}
+						</Box>
+					)}
+					{!news.description && (
+						<em>No description</em>
+					)}
+				</Box>
+
+			</Box>
+			<sub>
+				By {news.author}, {formattedDate} - <Link href={'/news/' + news.id}>
+					<a>Edit</a>
+				</Link>
+			</sub>
 		</Box>
 	)
 }
